917d5545e9b7841c4f494fa36f29946e8b3ad551
Savinu <savinu.hsa@gmail.com>
2026-01-28 16:25:39 +0530

    [Feature] Add OnboardingIntroView, OnboardingColorView

---
+++ AiChatApp/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,48 @@
+//
+//  OnboardingIntroView.swift
+//  AiChatApp
+//
+//  Created by Savinu Hasalanka on 28/01/2026.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        VStack {
+            Group {
+                Text("Make your own ")
+                +
+                Text("avatars ")
+                    .foregroundStyle(Color.accentColor)
+                    .fontWeight(.semibold)
+                +
+                Text("and chat with them!\n\nHave ")
+                +
+                Text("real conversations ")
+                    .foregroundStyle(Color.accentColor)
+                    .fontWeight(.semibold)
+                +
+                Text("with AI generated responses.")
+            }
+            .baselineOffset(6)
+            .frame(maxHeight: .infinity)
+            .padding(24)
+            
+            NavigationLink {
+                OnboardingColorView()
+            } label: {
+                Text("Continue")
+                    .callToActionButton()
+            }
+        }
+        .padding(24)
+        .font(.title3)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingIntroView()
+    }
+}
