254e12c5b48eebe253ef8d10ba532fa2bc110213
Savinu <savinu.hsa@gmail.com>
2026-01-28 17:02:55 +0530

    [Feature] OnboardingCompletedView

--- AiChatApp/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AiChatApp/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -11,27 +11,60 @@ struct OnboardingCompletedView: View {
     
     @Environment(AppState.self) private var root
     
+    @State var isCompletingProfileSetup: Bool = false
+    var selectedColor: Color = .orange
+    
     var body: some View {
-        VStack {
-            Text("Onboarding Completed!")
-                .frame(maxHeight: .infinity)
-            
-            Button {
-                onFinishButtonPressed()
-            } label: {
-                Text("Finish")
-                    .callToActionButton()
-            }
+        VStack (alignment: .leading, spacing: 12) {
+            Text("Setup Complete!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(selectedColor)
+            Text("We've set up your profile and you're ready to start chatting.")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundStyle(Color.secondary)
+
         }
+        .frame(maxHeight: .infinity)
+        .safeAreaInset(edge: .bottom, content: {
+            ctaButton
+        })
+        .padding(24)
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private var ctaButton: some View {
+        Button {
+            onFinishButtonPressed()
+        } label: {
+            ZStack {
+                if isCompletingProfileSetup {
+                    ProgressView()
+                        .tint(Color.white)
+                } else {
+                    Text("Finish")
+                }
+            }
+            .callToActionButton()
+        }
+        .disabled(isCompletingProfileSetup)
     }
     
     func onFinishButtonPressed() {
+        isCompletingProfileSetup = true
         // other logic to complete onboarding
-        root.updateViewState(showTabBarView: true)
+        
+        Task {
+            try await Task.sleep(for: .seconds(3))
+            isCompletingProfileSetup = false
+
+            root.updateViewState(showTabBarView: true)
+        }
     }
 }
 
 #Preview {
-    OnboardingCompletedView()
+    OnboardingCompletedView(selectedColor: .green)
         .environment(AppState())
 }
