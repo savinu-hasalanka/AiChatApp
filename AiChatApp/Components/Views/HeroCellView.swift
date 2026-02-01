ab4063eb32eb45152a12319e15a8f12df267075e
Savinu <savinu.hsa@gmail.com>
2026-02-01 14:37:18 +0530

    [Feature] HereCellView

---
+++ AiChatApp/Components/Views/HeroCellView.swift
@@ -0,0 +1,78 @@
+//
+//  HeroCellView.swift
+//  AiChatApp
+//
+//  Created by Savinu Hasalanka on 01/02/2026.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    
+    var title: String? = "This is some title"
+    var subtitle: String? = "This is some subtitle"
+    var imageName: String? = Constants.randomImage
+    
+    var body: some View {
+        ZStack {
+            if let imageName {
+                ImageLoaderView(urlString: imageName)
+            } else {
+                Rectangle()
+                    .fill(Color.accent)
+            }
+        }
+        .overlay(alignment: .bottomLeading, content: {
+            VStack(alignment: .leading, spacing: 4) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.subheadline)
+                }
+            }
+            .foregroundStyle(Color.white)
+            .padding(16)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(
+                LinearGradient(
+                    colors: [
+                        Color.black.opacity(0),
+                        Color.black.opacity(0.3),
+                        Color.black.opacity(0.4)
+                    ],startPoint: .top, endPoint: .bottom)
+            )
+        })
+        .cornerRadius(16)
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack {
+            HeroCellView()
+                .frame(width: 300, height: 200)
+            
+            HeroCellView()
+                .frame(width: 300, height: 400)
+            
+            HeroCellView()
+                .frame(width: 200, height: 400)
+            
+            HeroCellView(imageName: nil)
+                .frame(width: 300, height: 200)
+            
+            HeroCellView(title: nil)
+                .frame(width: 300, height: 200)
+            
+            HeroCellView(subtitle: nil)
+                .frame(width: 300, height: 200)
+            
+            HeroCellView(title: nil, subtitle: nil)
+                .frame(width: 300, height: 200)
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
