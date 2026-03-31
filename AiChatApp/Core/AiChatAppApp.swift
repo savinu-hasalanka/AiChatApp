069806a4a231eb8a865a338c4299922e5b1e95ee
Savinu <savinu.hsa@gmail.com>
2026-03-31 14:58:17 +0530

    [Feature] Multiple firebase info plist

--- AiChatApp/Core/AiChatAppApp.swift
+++ AiChatApp/Core/AiChatAppApp.swift
@@ -30,23 +30,42 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   
     func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        
-        FirebaseApp.configure()
+               
+        let config: BuildConfiguration
         
         #if MOCK
-        dependencies = Dependencies(config: .mock(isSignedIn: true))
+        config = .mock(isSignedIn: true)
         #elseif DEV
-        dependencies = Dependencies(config: .dev)
+        config = .dev
         #else
-        dependencies = Dependencies(config: .prod)
+        config = .prod
         #endif
         
+        config.configure()
+        dependencies = Dependencies(config: config)
+
         return true
   }
 }
 
 enum BuildConfiguration {
     case mock(isSignedIn: Bool), dev, prod
+    
+    func configure() {
+        switch self {
+        case .mock(_):
+            // mock does NOT run on Firebase
+            break
+        case .dev:
+            let plist = Bundle.main.path(forResource: "GoogleService-Info-Dev", ofType: "plist")!
+            let options = FirebaseOptions(contentsOfFile: plist)!
+            FirebaseApp.configure(options: options)
+        case .prod:
+            let plist = Bundle.main.path(forResource: "GoogleService-Info-Prod", ofType: "plist")!
+            let options = FirebaseOptions(contentsOfFile: plist)!
+            FirebaseApp.configure(options: options)
+        }
+    }
 }
 
 struct Dependencies {
