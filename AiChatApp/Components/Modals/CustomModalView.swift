9d0df40ec51cd1343ff8558c5d490aced62a337a
Savinu <savinu.hsa@gmail.com>
2026-05-12 12:01:18 +0530

    [Feature] Add App Store rating logic

---
+++ AiChatApp/Components/Modals/CustomModalView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomModalView.swift
+//  AiChatApp
+//
+//  Created by Savinu Hasalanka on 12/05/2026.
+//
+
+import SwiftUI
+
+struct CustomModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomModalView()
+}
